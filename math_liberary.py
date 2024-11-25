89cd94ede30e370f09575c82e40054b88cf88d1e
Fateme Baneshi <fbaneshi@pa.uc3m.es>
2024-11-25 11:50:56 +0100

    newwww

--- math_liberary.py
+++ math_liberary.py
@@ -12,4 +12,23 @@ def scalar_product(vector1, vector2):
 
     return sum(x * y for x, y in zip(vector1, vector2))
 
-gg
+
+    def vector_product(vector1, vector2):
+        """
+        Calculate the vector product (cross product) of two 3-dimensional vectors.
+
+        :param vector1: List of 3 numbers representing the first vector.
+        :param vector2: List of 3 numbers representing the second vector.
+        :return: Vector product of the two vectors as a list of 3 numbers.
+        """
+        if len(vector1) != 3 or len(vector2) != 3:
+            raise ValueError("Both vectors must be of length 3")
+
+        x1, y1, z1 = vector1
+        x2, y2, z2 = vector2
+
+        return [
+            y1 * z2 - z1 * y2,
+            z1 * x2 - x1 * z2,
+            x1 * y2 - y1 * x2
+        ]
